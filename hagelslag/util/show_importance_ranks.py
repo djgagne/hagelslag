2bd3d87e5f1022c9f6d19d86a9a39c8698c12942
David John Gagne <djgagne@ou.edu>
2015-12-04 11:45:29 -0600

    Added feature importance ranking script.

---
+++ hagelslag/util/show_importance_ranks.py
@@ -0,0 +1,29 @@
+import cPickle
+import numpy as np
+from hagelslag.util.Config import Config
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", required=True, help="Config file containing input columns")
+    parser.add_argument("-m", "--model", required=True, help="List of machine learning models with feature importances")
+    parser.add_argument("-n", "--num", type=int, default=10, help="Number of features to display")
+    args = parser.parse_args()
+    config = Config(args.config, required_attributes=["input_columns"])
+    input_columns = np.array(config.input_columns)
+    model_files = args.model.split(",")
+    for model_file in model_files:
+        model_fo = open(model_file)
+        model_obj = cPickle.load(model_fo)
+        model_fo.close()
+        if hasattr(model_obj, "feature_importances_"):
+            importances = model_obj.feature_importances_
+            ranking = np.argsort(importances)[::-1]
+            for r, rank in enumerate(ranking[:args.num]):
+                print("{0:2d} {1} {2:1.3f}".format(r, input_columns[rank], importances[rank]))
+    return
+
+
+if __name__ == "__main__":
+    main()
