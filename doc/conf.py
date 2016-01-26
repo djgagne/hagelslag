7719879aeaccdfbc4a37e0b7ca9febea04e504db
David John Gagne <djgagne@ou.edu>
2016-01-25 21:33:22 -0600

    Added more mock imports for submodules.

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,10 @@ class Mock(MagicMock):
             return Mock()
 
 
-MOCK_MODULES = ['numpy', 'scipy', 'pandas', 'skimage.morphology', 'netCDF4', 'basemap', 'matplotlib', "pyproj",
-                "sklearn", "scipy.ndimage", "matplotlib.pyplot", "scipy.stats", "scipy.signal", "skimage.measure",
-                "skimage.segmentation", "scipy.interpolate", "mpl_toolkits.axes_grid.inset_locator"]
+MOCK_MODULES = ['numpy', 'scipy', 'pandas', 'skimage', 'netCDF4', 'basemap', 'matplotlib', "pyproj", "Nio",
+                "mpl_toolkits", "mpl_toolkits.axes_grid",
+                "sklearn", 'skimage.morphology', "scipy.ndimage", "matplotlib.pyplot", "scipy.stats", "scipy.signal",
+                "skimage.measure", "skimage.segmentation", "scipy.interpolate", "mpl_toolkits.axes_grid.inset_locator"]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
