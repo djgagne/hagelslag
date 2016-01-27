8173ade298493acc1369079b7649260c58945bc3
David Gagne <djgagne@ou.edu>
2016-01-27 16:13:40 -0700

    Updated ncar forecast file to handle multiple training periods

--- hagelslag/data/ModelGrid.py
+++ hagelslag/data/ModelGrid.py
@@ -104,9 +104,9 @@ class ModelGrid(object):
         if len(self.file_objects) == 1 and self.file_objects[0] is not None:
             var_name, z_index = self.format_var_name(self.variable, self.file_objects[0].variables.keys())
             if z_index is None:
-                data = self.file_objects[0].variables[self.variable][self.forecast_hours]
+                data = self.file_objects[0].variables[var_name][self.forecast_hours]
             else:
-                data = self.file_objects[0].variables[self.variable][self.forecast_hours, z_index]
+                data = self.file_objects[0].variables[var_name][self.forecast_hours, z_index]
             if hasattr(self.file_objects[0].variables[var_name], "units"):
                 units = self.file_objects[0].variables[var_name].units
         elif len(self.file_objects) > 1:
