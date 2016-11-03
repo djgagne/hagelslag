c47eb50caa3b1b34a1994c1aa7aa98d0c332bcc4
ahijevyc <ahijevyc@ucar.edu>
2016-11-03 16:00:24 -0600

    replaced tabs with space indents

--- hagelslag/data/ModelOutput.py
+++ hagelslag/data/ModelOutput.py
@@ -145,14 +145,14 @@ class ModelOutput(object):
             self.i, self.j = np.indices(self.lon.shape)
             self.proj = get_proj_obj(proj_dict)
         elif self.ensemble_name.upper() == "NCAR" or self.ensemble_name.upper() == "VSE":
-	    proj_dict, grid_dict = read_ncar_map_file(map_file)
-	    if self.member_name[0:7] == "1km_pbl": # Don't just look at the first 3 characters. You have to differentiate '1km_pbl1' and '1km_on_3km_pbl1'
-		grid_dict["dx"] = 1000
-		grid_dict["dy"] = 1000
-		grid_dict["sw_lon"] = 258.697
-		grid_dict["sw_lat"] = 23.999
-		grid_dict["ne_lon"] = 282.868269206236
-		grid_dict["ne_lat"] = 36.4822338520542 
+            proj_dict, grid_dict = read_ncar_map_file(map_file)
+            if self.member_name[0:7] == "1km_pbl": # Don't just look at the first 3 characters. You have to differentiate '1km_pbl1' and '1km_on_3km_pbl1'
+                grid_dict["dx"] = 1000
+                grid_dict["dy"] = 1000
+                grid_dict["sw_lon"] = 258.697
+                grid_dict["sw_lat"] = 23.999
+                grid_dict["ne_lon"] = 282.868269206236
+                grid_dict["ne_lat"] = 36.4822338520542 
 
             self.dx = int(grid_dict["dx"])
             mapping_data = make_proj_grids(proj_dict, grid_dict)
