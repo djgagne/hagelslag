1e0832074bcfd8b46f9c3b17f8d6741ecc302a28
Amanda Burke <aburke@nimbus.capsint>
2020-01-05 16:05:16 -0600

    Continue updating HREFv2 input

--- hagelslag/data/HREFv2ModelGrid.py
+++ hagelslag/data/HREFv2ModelGrid.py
@@ -26,36 +26,39 @@ class HREFv2ModelGrid(Grib_ModelGrid):
         filenames = []
         self.forecast_hours = np.arange((start_date - run_date).total_seconds() / 3600,
                                         (end_date - run_date).total_seconds() / 3600 + 1, dtype=int)
-        
         day_before_date = (run_date-timedelta(days=1)).strftime("%Y%m%d") 
         member_name = str(self.member.split("_")[0])
         if 'nam' in self.member:
             if '00' in self.member:
                 for forecast_hr in self.forecast_hours:
-                    files = glob.glob('{0}/{1}/nam_conusnest_*00*{2}*'.format(
-                                    self.path,run_date.strftime("%y%m%d"),forecast_hr))
+                    files = glob.glob('{0}/{1}/nam_conusnest*00*{2}*'.format(
+                                    self.path,run_date.strftime("%Y%m%d"),forecast_hr))
                     if not files: 
                         files = glob.glob('{0}/{1}/nam.t00z*{2}*'.format(
-                                    self.path,run_date.strftime("%y%m%d"),forecast_hr))
-                    filenames.append(files[0])
+                                    self.path,run_date.strftime("%Y%m%d"),forecast_hr))
+                    if files:
+                        filenames.append(files[0])
             elif '12' in self.member:
                 for forecast_hr in self.forecast_hours:
-                    files = glob.glob('{0}/{1}/nam_conusnest_*12*{2}*'.format(
+                    files = glob.glob('{0}/{1}/nam_conusnest*12*{2}*'.format(
                                     self.path,day_before_date,(forecast_hr+12)))
                     if not files: 
                         files = glob.glob('{0}/{1}/nam.t12z*{2}*'.format(
                                     self.path,day_before_date,(forecast_hr+12)))
-                    filenames.append(files[0])
+                    if files:
+                        filenames.append(files[0])
         else:
             if '00' in self.member:
                 for forecast_hr in self.forecast_hours:
-                    files = glob.glob('{0}/{1}/hiresw_conus*{2}*00*{3}*'.format(
-                            self.path,run_date.strftime("%y%m%d"),member_name,forecast_hr))
-                    filenames.append(files[0])
+                    files = glob.glob('{0}/{1}/hiresw_conus{2}*00*{3}*'.format(
+                            self.path,run_date.strftime("%Y%m%d"),member_name,forecast_hr))
+                    if files:
+                        filenames.append(files[0])
             elif '12' in self.member:
                 for forecast_hr in self.forecast_hours:
-                    files = glob.glob('{0}/{1}/hiresw_conus*{2}*12*{3}*'.format(
+                    files = glob.glob('{0}/{1}/hiresw_conus{2}*12*{3}*'.format(
                             self.path,day_before_date,member_name,(forecast_hr+12)))
-                    filenames.append(files[0])
+                    if files:
+                        filenames.append(files[0])
         super(HREFv2ModelGrid, self).__init__(filenames,run_date,start_date,end_date,variable,member)
         return 
