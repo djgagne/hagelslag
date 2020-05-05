885d55f3dc49682ec5f30c4e808130cd0c5e327f
djgagne@ou.edu <djgagne@ou.edu>
2020-05-05 13:19:51 -0600

    Updated ModelOutput module to handle new NCARWRF2020 format

--- hagelslag/data/ModelOutput.py
+++ hagelslag/data/ModelOutput.py
@@ -6,6 +6,7 @@ from .HRRRModelGrid import HRRRModelGrid
 from .HRRREModelGrid import HRRREModelGrid
 from .HREFv2ModelGrid import HREFv2ModelGrid
 from .NCARStormEventModelGrid import NCARStormEventModelGrid
+from .NCARWRF2020ModelGrid import NCARWRF2020ModelGrid
 from hagelslag.util.make_proj_grids import make_proj_grids, read_arps_map_file, read_ncar_map_file, get_proj_obj
 from hagelslag.util.derived_vars import relative_humidity_pressure_level, melting_layer_height
 import numpy as np
@@ -170,6 +171,16 @@ class ModelOutput(object):
                                          self.path)
             self.data, self.units = mg.load_data()
             mg.close()
+        elif self.ensemble_name.upper() == "NCARWRF2020":
+            mg = NCARWRF2020ModelGrid(self.member_name,
+                                      self.run_date,
+                                      self.variable,
+                                      self.start_date,
+                                      self.end_date,
+                                      self.path)
+            self.data, self.units = mg.load_data()
+            mg.close()
+
         else:
             print(self.ensemble_name + " not supported.")
 
@@ -188,7 +199,7 @@ class ModelOutput(object):
                 setattr(self, m, v)
             self.i, self.j = np.indices(self.lon.shape)
             self.proj = get_proj_obj(proj_dict)
-        elif self.ensemble_name.upper() in ["NCAR", "NCARSTORM", "HRRR", "VSE", "HREFV2","HRRRE"]:
+        elif self.ensemble_name.upper() in ["NCAR", "NCARSTORM", "NCARWRF2020", "HRRR", "VSE", "HREFV2","HRRRE"]:
             proj_dict, grid_dict = read_ncar_map_file(map_file)
             if self.member_name[0:7] == "1km_pbl": # Don't just look at the first 3 characters. You have to differentiate '1km_pbl1' and '1km_on_3km_pbl1'
                 grid_dict["dx"] = 1000
@@ -198,6 +209,14 @@ class ModelOutput(object):
                 grid_dict["ne_lon"] = 282.868269206236
                 grid_dict["ne_lat"] = 36.4822338520542 
 
+            self.dx = int(grid_dict["dx"])
+            mapping_data = make_proj_grids(proj_dict, grid_dict)
+            for m, v in mapping_data.items():
+                setattr(self, m, v)
+            self.i, self.j = np.indices(self.lon.shape)
+            self.proj = get_proj_obj(proj_dict)
+        else:
+            proj_dict, grid_dict = read_ncar_map_file(map_file)
             self.dx = int(grid_dict["dx"])
             mapping_data = make_proj_grids(proj_dict, grid_dict)
             for m, v in mapping_data.items():
--- hagelslag/data/NCARWRF2020ModelGrid.py
+++ hagelslag/data/NCARWRF2020ModelGrid.py
@@ -9,16 +9,18 @@ class NCARWRF2020ModelGrid(ModelGrid):
     Loads model output from the NCAR MMM WRF 2020 3 km real-time runs
 
     """
-    def __init__(self, run_date, variable, start_date, end_date, path):
+    def __init__(self, member_name, run_date, variable, start_date, end_date, path):
+        self.member_name = member_name
         self.pressure_levels = np.array([1000, 925, 850, 700, 600, 500, 400, 300, 250, 200, 150, 100])
         self.path = path
         self.forecast_hours = np.arange((start_date - run_date).total_seconds() / 3600,
                                         (end_date - run_date).total_seconds() / 3600 + 1, dtype=int)
         filenames = []
         for hour in self.forecast_hours:
-            filename = join(path, run_date.strftime("%Y%m%d%H"),
+            filename = join(path, run_date.strftime("%Y%m%d%H"), member_name,
                             "diags_d01_f{0:03d}.nc".format(hour))
             filenames.append(filename)
+            print(filename)
         super(NCARWRF2020ModelGrid, self).__init__(filenames, run_date, start_date, end_date, variable)
 
     def format_var_name(self, variable, var_list):
